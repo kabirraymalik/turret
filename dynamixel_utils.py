83c67d9223a7b07694e2e6ae65998b31f60a44e8
Kabir Ray Malik <kabirraymalik@gmail.com>
2024-07-26 03:49:28 -0400

    dynamanager is working with support for XL430 and XL330 motors, now onto kinematics

--- dynamixel_utils.py
+++ dynamixel_utils.py
@@ -2,46 +2,34 @@ from dynamixel_sdk import *
 import sys, tty, termios, time
 import numpy as np
 
-fd = sys.stdin.fileno()
-old_settings = termios.tcgetattr(fd)
+class DynaManager():
+    def __init__(self, device_name, connected_motors):
+        self.fd = sys.stdin.fileno()
+        self.old_settings = termios.tcgetattr(self.fd)
 
-def getch():
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
-
-class Dynamixel_Helper():
-    def __init__(self, device_name):
-
-        MY_DXL = 'X_SERIES'       # X330 (5.0 V recommended), X430, X540, 2X430
-        # MY_DXL = 'XL320'        # [WARNING] Operating Voltage : 7.4V
-
-
-        # Control table address
-        if MY_DXL == 'X_SERIES' or MY_DXL == 'MX_SERIES':
-            self.ADDR_TORQUE_ENABLE          = 64
-            self.ADDR_GOAL_POSITION          = 116
-            self.ADDR_GOAL_CURRENT           = 102
-            self.ADDR_PRESENT_POSITION       = 132
-            self.ADDR_PRESENT_VELOCITY = 128
-            self.ADDR_PRESENT_CURRENT = 126
-            self.ADDR_OPERATING_MODE = 11
-            self.ADDR_GOAL_VELOCITY = 104
-            GOAL_VELOCITY1 = 80 #max 265 for 430s
-            GOAL_VELOCITY2 = 80
-            self.DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the Minimum Position Limit of product eManual
-            self.DXL_MAXIMUM_POSITION_VALUE  = 4095      # Refer to the Maximum Position Limit of product eManual
-            BAUDRATE                    = 57600
-        elif MY_DXL == 'XL320':
-            ADDR_TORQUE_ENABLE          = 24
-            ADDR_GOAL_POSITION          = 30
-            ADDR_PRESENT_POSITION       = 37
-            DXL_MINIMUM_POSITION_VALUE  = 0         # Refer to the CW Angle Limit of product eManual
-            DXL_MAXIMUM_POSITION_VALUE  = 1023      # Refer to the CCW Angle Limit of product eManual
-            BAUDRATE                    = 1000000   # Default Baudrate of XL-320 is 1Mbps
+        #tracks various motor types in system
+        self.motors = connected_motors
+        #Dynamixel motor pin info
+        xl430_info = {'enable_torque': 64,          'set_position': 116,
+                    'set_velocity_P': 78,           'get_position': 132, 
+                    'get_velocity': 128,            'get_current': 126,
+                    'access_operating_mode': 11,    'set_velocity': 104,
+                    'max_velocity': 265,            'max_position': 4095,
+                    'baudrate': 57600,              'set_position_velocity': 112,
+                    'set_position_P': 84,           'set_position_I': 82,
+                    'set_position_D': 80,           'set_velocity_I': 76}
+        
+        xl330_info = {'enable_torque': 64,          'set_position': 116,
+                      'set_current': 102,           'get_position': 132, 
+                      'get_velocity': 128,          'get_current': 126,
+                      'access_operating_mode': 11,  'set_velocity': 104,
+                      'max_velocity': 445,          'max_position': 4095,
+                      'baudrate': 57600,            'set_position_velocity': 112,
+                      'set_position_P': 84,         'set_position_I': 82,
+                      'set_position_D': 80,         'set_velocity_I': 76,
+                      'set_velocity_P': 78}
+        
+        self.motor_info = {'XL430': xl430_info, 'XL330': xl330_info}
 
         # DYNAMIXEL Protocol Version (1.0 / 2.0)
         # https://emanual.robotis.com/docs/en/dxl/protocol2/
@@ -55,21 +43,47 @@ class Dynamixel_Helper():
         else:
             print("Failed to open the port")
             print("Press any key to terminate...")
-            getch()
+            self.getch()
             quit()
 
         # Set port baudrate
-        if self.portHandler.setBaudRate(BAUDRATE):
+        if self.portHandler.setBaudRate(int(self.motor_info[self.motors[0]].get('baudrate'))):
             print("Succeeded to change the baudrate")
         else:
             print("Failed to change the baudrate")
             print("Press any key to terminate...")
-            getch()
+            self.getch()
             quit()
+
+    #======================================= UTILS ===========================================#
+
+    def get_motor_info(self, motor_ID, cmd):
+        if self.motors[motor_ID-1] in self.motor_info:
+
+            info = self.motor_info[self.motors[motor_ID-1]]
+            if cmd in info.keys():
+                return info[cmd]
+            else:
+                print('ERROR: invalid command for selected motor!')
+                print(f'motor: {motor_ID} command: {cmd}')
+                return -1
+        else:
+            print(f'ERROR: invalid motor type: {self.motors[motor_ID-1]}')
+            return -1
+
+    def getch(self):
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old_settings)
+        return ch
+        
+    #==============================INDIVIDUAL MOTOR FUNCTIONS=================================#
 
     def enable_torque(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_TORQUE_ENABLE, 1)
+            self.portHandler, motor_ID, self.get_motor_info(motor_ID,'enable_torque'), 1)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -79,7 +93,7 @@ class Dynamixel_Helper():
 
     def disable_torque(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_TORQUE_ENABLE, 0)
+            self.portHandler, motor_ID, self.get_motor_info(motor_ID,'enable_torque'), 0)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -89,7 +103,7 @@ class Dynamixel_Helper():
     
     def set_voltage_mode(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_OPERATING_MODE, 16)
+            self.portHandler, motor_ID, self.get_motor_info(motor_ID,'access_operating_mode'), 16)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -99,18 +113,17 @@ class Dynamixel_Helper():
 
     def set_position_mode(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_OPERATING_MODE, 3)
+            self.portHandler, motor_ID, self.get_motor_info(motor_ID,'access_operating_mode'), 3)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
-            print('dxl_error: {dxl_error}')
             print("%s" % self.packetHandler.getRxPacketError(dxl_error))
         else:
             print(f"Dynamixel {motor_ID} mode successfully changed to position")
 
     def set_velocity_mode(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_OPERATING_MODE, 1)
+            self.portHandler, motor_ID, self.get_motor_info(motor_ID,'access_operating_mode'), 1)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -120,7 +133,7 @@ class Dynamixel_Helper():
         
     def set_extended_position_control_mode(self, motor_ID):
         dxl_comm_result, dxl_error = self.packetHandler.write1ByteTxRx(
-            self.portHandler, motor_ID, self.ADDR_OPERATING_MODE, 4)
+            self.portHandler, motor_ID, self.get_motor_info('access_operating_mode'), 4)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -132,13 +145,14 @@ class Dynamixel_Helper():
         goal_vel = 265.0 * velocity_as_decimal
         if goal_vel <= 265:
             dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(
-                    self.portHandler, motor_ID, self.ADDR_GOAL_VELOCITY, int(goal_vel))
+                    self.portHandler, motor_ID, self.get_motor_info(motor_ID,'set_velocity'), int(goal_vel))
             if dxl_comm_result != COMM_SUCCESS:
                     print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
             elif dxl_error != 0:
                     print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+    
     def get_operating_mode(self, motor_ID):
-        dxl_operating_mode, dxl_comm_result, dxl_error = self.packetHandler.read1ByteTxRx(self.portHandler, motor_ID, 11)
+        dxl_operating_mode, dxl_comm_result, dxl_error = self.packetHandler.read1ByteTxRx(self.portHandler, motor_ID, self.get_motor_info(motor_ID,'access_operating_mode'))
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -147,46 +161,59 @@ class Dynamixel_Helper():
             print(f'motor {motor_ID} operating mode: {dxl_operating_mode}')
             return dxl_operating_mode
         
+    def set_position(self, motor_ID, position_in_radians):
+        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, motor_ID, self.get_motor_info(motor_ID,'set_position'), int((self.DXL_MAXIMUM_POSITION_VALUE * position_in_radians/(2 * np.pi))))
+        if dxl_comm_result != COMM_SUCCESS:
+            print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % self.packetHandler.getRxPacketError(dxl_error))
+
     def get_position(self, motor_ID):
-        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, motor_ID, self.ADDR_PRESENT_POSITION)
+        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, motor_ID, self.get_motor_info(motor_ID,'get_position'))
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % self.packetHandler.getRxPacketError(dxl_error))
         else:
             return dxl_present_position
-        
-    def set_position(self, motor_ID, position_in_radians):
-        print(f'tried to move to position {int((self.DXL_MAXIMUM_POSITION_VALUE/(2 * np.pi)) * position_in_radians)}')
-        dxl_comm_result, dxl_error = self.packetHandler.write4ByteTxRx(self.portHandler, motor_ID, self.ADDR_GOAL_POSITION, int((self.DXL_MAXIMUM_POSITION_VALUE * position_in_radians/(2 * np.pi))))
+    
+    def set_current(self, motor_ID, current_as_decimal):
+        dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, motor_ID, self.get_motor_info(motor_ID,'set_current'), current_as_decimal*1750)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % self.packetHandler.getRxPacketError(dxl_error))
-    
+
     def get_current(self, motor_ID):
-        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, motor_ID, self.ADDR_PRESENT_CURRENT)
+        dxl_present_position, dxl_comm_result, dxl_error = self.packetHandler.read4ByteTxRx(self.portHandler, motor_ID, self.get_motor_info(motor_ID,'get_current'))
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % self.packetHandler.getRxPacketError(dxl_error))
         else:
             return (dxl_present_position/self.DXL_MAXIMUM_POSITION_VALUE) * 2 * np.pi
-        
-    def set_current(self, motor_ID, current_as_decimal):
-        if motor_ID < 3:
-            dxl_comm_result, dxl_error = self.packetHandler.write2ByteTxRx(self.portHandler, motor_ID, 102, current_as_decimal*1750)
-            if dxl_comm_result != COMM_SUCCESS:
-                print("%s" % self.packetHandler.getTxRxResult(dxl_comm_result))
-            elif dxl_error != 0:
-                print("%s" % self.packetHandler.getRxPacketError(dxl_error))
-        else: 
-            print('figure out current limits for smaller motors')
+    
+     #================================GROUP MOTOR FUNCTIONS====================================#
+
+    def enable_torque_all(self):
+        for motor_index in self.motors:
+            self.enable_torque(motor_index+1)
+    def disable_torque_all(self):
+        for motor_index in self.motors:
+            self.disable_torque(motor_index+1)
+    def shut_down(self):
+        self.disable_torque_all()
+        self.portHandler.closePort()  
+
     def go_home(self):
         self.disable_torque(1)
         self.disable_torque(2)
+        self.disable_torque(3)
+        self.disable_torque(4)
         self.set_position_mode(1)
         self.set_position_mode(2)
+        self.set_position_mode(3)
+        self.set_position_mode(4)
         self.enable_torque(1)
         self.enable_torque(2)
         self.set_position(1,np.pi)
@@ -194,35 +221,20 @@ class Dynamixel_Helper():
     def active(self):
         self.set_position(1,3*np.pi/2)
         self.set_position(2, np.pi/3)
-
-helper = Dynamixel_Helper(device_name="/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT94VY5B-if00-port0")
-helper.go_home()
-#loop initialization 
-base_refresh_rate = 100 #Hz
-start_time = time.time()
-last_refresh = start_time
-stopped = False
-first_time = True
-while not stopped:
-    if time.time()-start_time > 5:
-        if first_time:
-            helper.active()
-            #helper.set_velocity(2, 0.4)
-            first_time = False
-    if time.time()-start_time>10:
-        stopped = True
-
-    print(f'current position 1: {helper.get_position(1)}')
-    #print(f'current position 2: {helper.get_position(2)}')
-
-    #time control 
-    while (time.time()) < (last_refresh + 1.0/base_refresh_rate):
-        time.sleep(0.00001)
-    curr_time = time.time()
-    time_elapsed = curr_time - last_refresh
-    last_refresh = curr_time
-    print(f'refresh rate: {1.0/time_elapsed} Hz')
-helper.go_home()
-time.sleep()
-helper.disable_torque(1)
-helper.disable_torque(2)
+    def test1(self):
+        self.disable_torque(1)
+        self.disable_torque(4)
+        self.disable_torque(5)
+        self.set_position_mode(1)
+        self.set_position_mode(4)
+        self.set_position_mode(5)
+        self.enable_torque(1)
+        self.enable_torque(4)
+        self.enable_torque(5)
+        self.set_position(1, 0.01)
+        self.set_position(4, 0.01)
+        self.set_position(5, 0.01)
+    def test2(self):
+        self.set_position(1, np.pi)
+        self.set_position(4, np.pi)
+        self.set_position(5, np.pi)
--- main.py
+++ main.py
@@ -1,20 +1,26 @@
-import os, time
-from DynamixelSDK.python.src import *
+from dynamixel_utils import *
 
-def main():
-    refresh_rate = 100 #Hz
-    start_time = time.time()
-    last_refresh = start_time
-    stopped = False
-    while not stopped:
-        #TODO: do something here
-        #time control 
-        while (time.time()) < (last_refresh + 1.0/refresh_rate):
-            time.sleep(0.00001)
-        curr_time = time.time()
-        time_elapsed = curr_time - last_refresh
-        last_refresh = curr_time
-        print(f'refresh rate: {1.0/time_elapsed} Hz')
+motors = ['XL430', 'XL430', 'XL430', 'XL430', 'XL330', 'XL330']
+dm = DynaManager(device_name="/dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT5NUSV6-if00-port0", connected_motors = motors)
+
+#loop initialization 
+base_refresh_rate = 100 #Hz
+start_time = time.time()
+last_refresh = start_time
+stopped = False
+dm.enable_torque_all()
+while not stopped:
+    #runtime limit 10s
+    if time.time()-start_time>10:
+        stopped = True
 
 
-main()
+    #time control 
+    while (time.time()) < (last_refresh + 1.0/base_refresh_rate):
+        time.sleep(0.00001)
+    curr_time = time.time()
+    time_elapsed = curr_time - last_refresh
+    last_refresh = curr_time
+    print(f'refresh rate: {1.0/time_elapsed} Hz')
+dm.shut_down()
+

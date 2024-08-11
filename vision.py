8924dad48cb44b74db8d95ff78879235b3a656bc
Kabir Ray Malik <kabirraymalik@gmail.com>
2024-08-11 02:17:38 -0400

    more vision stuff, jetson compatibility work?

--- vision.py
+++ vision.py
@@ -1,6 +1,38 @@
 import cv2, time
 import numpy
 
+
+def __gstreamer_pipeline(
+    camera_id,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=1920,
+    display_height=1080,
+    framerate=30,
+    flip_method=0,
+    ):
+    return (
+            "nvarguscamerasrc sensor-id=%d ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink max-buffers=1 drop=True"
+            % (
+                    camera_id,
+                    capture_width,
+                    capture_height,
+                    framerate,
+                    flip_method,
+                    display_width,
+                    display_height,
+            )
+    )
+   
+#cap = cv2.VideoCapture(__gstreamer_pipeline(camera_id=0, flip_method=2), cv2.CAP_GSTREAMER)
+
 cap = cv2.VideoCapture(0)
 
 base_refresh_rate = 30 #Hz
